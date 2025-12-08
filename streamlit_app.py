0d193501444fa4ac235be5faac173c7cfdcd7e3b
Jamz1986 <162839451+Jamz1986@users.noreply.github.com>
2025-12-08 17:45:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 Kallpa Securities')
 
-st.write('Hello world!')
+st.info('Este es un aplicativo LSTM')
+
+with st.expander('Data'):
+   st.write('**Raw Data**')
+   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+   df
